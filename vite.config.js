258ba23457b1712228be50bd469d85cc10b31ed0
Loyyd <konradkunkel@gmx.de>
2025-06-16 18:51:56 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Flying_Cube/', // Replace with your repository name
   plugins: [
     {
       name: 'html-full-reload',
@@ -20,4 +18,5 @@ export default defineConfig({
       },
     }
   ],
+  base: '/Flying_Cube/',
 });
