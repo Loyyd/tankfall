5ea24c17957f473d3506afbe97cae45a9acc05ad
Loyyd <konradkunkel@gmx.de>
2025-06-16 16:37:33 +0200

    push

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Flying_Cube/', // Replace with your repository name
   plugins: [
     {
       name: 'html-full-reload',
